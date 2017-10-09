9050d5c839db39c311501c52ef5f96af6cd55dee
Brittany Martin <brittany.jill.martin@gmail.com>
2017-10-09 11:10:05 -0400

    Setup Slack for the bot and configure the app for verification and responding to messages

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const qs = require('querystring');
 
 module.exports.endpoint = (event, context, callback) => {
   const request = {
-    token: <SLACK TOKEN>
+    token: "xoxb-253596753765-DlcBvw3ICLiYaKUu48uQMdET"
   }
 
   const url = 'https://slack.com/api/users.list?' + qs.stringify(request);
--- handler.js
+++ handler.js
@@ -1,16 +1,49 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const https = require('https');
+const qs = require('querystring');
 
-  callback(null, response);
+const direct_mention = new RegExp('^\<\@' + process.env.BOT_ID + '\>', 'i');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+module.exports.endpoint = (event, context, callback) => {
+  console.log('Received event', event);
+
+  const request = JSON.parse(event.body);
+
+  if (request.type === 'url_verification' && request.token === process.env.VERIFICATION_TOKEN) {
+      console.log('Verifying url');
+
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          'challenge' : request.challenge
+        })
+      }
+
+      return callback(null, response);
+  }
+
+  if (request.event.text.match(direct_mention)) {
+    console.log('Echoing message back to slack');
+
+    const response = {
+      token: process.env.BOT_ACCESS_TOKEN,
+      channel: request.event.channel,
+      text: request.event.text.replace(direct_mention, '') + ' [' + Date.now() + ']'
+    }
+
+    const URL = process.env.POST_MESSAGE_URL + qs.stringify(response);
+
+    https.get(URL, (res) => {
+      const statusCode = res.statusCode;
+
+      console.log(direct_mention, request.event.text.replace(direct_mention, ''), statusCode);
+
+      if (statusCode !== 200) {
+        console.log(res);
+      }
+
+      callback(null, {statusCode: 200});
+    })
+  }
 };
