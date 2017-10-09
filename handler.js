69f23c2e79cf7d9a406d263c5d0da3fe3969990a
Brittany Martin <brittany.jill.martin@gmail.com>
2017-10-09 11:44:05 -0400

    Building an image processor from Slack to AWS

--- handler.js
+++ handler.js
@@ -1,49 +1,114 @@
 'use strict';
 
 const https = require('https');
+const fs = require('fs');
+const aws = require('aws-sdk');
 const qs = require('querystring');
+const s3 = new aws.S3();
 
-const direct_mention = new RegExp('^\<\@' + process.env.BOT_ID + '\>', 'i');
+const downloadFileToSystem = function (path, filename) {
+    console.log('Downloading image to temp storage');
+
+    const file = fs.createWriteStream(process.env.TEMP_FOLDER + filename);
+
+    const options = {
+        hostname: process.env.SLACK_HOSTNAME,
+        path: path,
+        headers: {
+            authorization: 'Bearer ' + process.env.BOT_ACCESS_TOKEN
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        const request = https.get(options, (response) => {
+
+            if (response.statusCode < 200 || response.statusCode > 299) {
+                reject(new Error('Failure downloading file: ' + response.statusCode));
+            }
+
+            response.pipe(file);
+
+            file.on('finish', function () {
+                file.close(() => resolve());
+            });
+        });
+
+        request.on('error', (err) => reject(err))
+    })
+};
+
+const uploadToBucket = function (filename) {
+    console.log('Uploading image to S3');
+
+    const bodystream = fs.createReadStream(process.env.TEMP_FOLDER + filename);
+
+    return new Promise((resolve, reject) => {
+        s3.putObject({
+            Bucket: process.env.UPLOAD_BUCKET,
+            Key: filename,
+            Body: bodystream
+        }, function (error, data) {
+            if (error) {
+                return reject(error);
+            }
+            return resolve();
+        });
+    });
+};
+
+const updateStatusInSlack = function (filename, channel) {
+    console.log('Sending status message to slack');
+
+    return new Promise((resolve, reject) => {
+        const response = {
+            token: process.env.BOT_ACCESS_TOKEN,
+            channel: channel,
+            text: 'I am working on ' + filename + '... should be done soon.'
+        }
+
+        const URL = process.env.POST_MESSAGE_URL + qs.stringify(response);
+
+        https.get(URL, (res) => {
+            const statusCode = res.statusCode;
+            resolve();
+        })
+    });
+}
 
 module.exports.endpoint = (event, context, callback) => {
-  console.log('Received event', event);
+    console.log('Received event', event);
 
-  const request = JSON.parse(event.body);
+    const request = JSON.parse(event.body);
 
-  if (request.type === 'url_verification' && request.token === process.env.VERIFICATION_TOKEN) {
-      console.log('Verifying url');
+    if (request.event.type && request.event.type === 'message' &&
+        request.event.subtype && request.event.subtype === 'file_share') {
 
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          'challenge' : request.challenge
-        })
-      }
+        console.log('Processing uploaded file');
 
-      return callback(null, response);
-  }
+        const path = request.event.file.url_private_download;
+        const filename = request.event.file.name;
+        const channel = request.event.channel;
 
-  if (request.event.text.match(direct_mention)) {
-    console.log('Echoing message back to slack');
+        downloadFileToSystem(path, filename)
+            .then(() => uploadToBucket(filename))
+            .then(() => updateStatusInSlack(filename, channel))
+            .then(() => {
+                console.log('Returning result')
+                callback(null, {
+                    statusCode: 200
+                })
+            })
+            .catch((err) => {
+                console.log('Error', err);
+                callback(null, {
+                    statusCode: 500
+                })
+            });
 
-    const response = {
-      token: process.env.BOT_ACCESS_TOKEN,
-      channel: request.event.channel,
-      text: request.event.text.replace(direct_mention, '') + ' [' + Date.now() + ']'
+        return;
     }
 
-    const URL = process.env.POST_MESSAGE_URL + qs.stringify(response);
-
-    https.get(URL, (res) => {
-      const statusCode = res.statusCode;
-
-      console.log(direct_mention, request.event.text.replace(direct_mention, ''), statusCode);
-
-      if (statusCode !== 200) {
-        console.log(res);
-      }
-
-      callback(null, {statusCode: 200});
-    })
-  }
+    callback(null, {
+        statusCode: 200
+    });
 };
